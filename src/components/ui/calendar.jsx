5fdbdf6336b3ad3e9fbd1efc99a81d980897b2c7
benjikir <b.becker94@web.de>
2025-09-19 20:58:20 +0200

    "added better calendar component and ui for trips function"

---
+++ src/components/ui/calendar.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+import * as React from "react";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+
+
+export function Calendar(props) {
+  return <DayPicker {...props} />;
+}
